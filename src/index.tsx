01705624adab5edab7b9b33fcf40d9ae50985211
kedMertens <kedmail@protonmail.com>
2026-03-14 22:28:55 +0200

    Adds button to lib view, no action yet

--- src/index.tsx
+++ src/index.tsx
@@ -1,115 +1,80 @@
 import {
   ButtonItem,
-  PanelSection,
-  PanelSectionRow,
-  Navigation,
-  staticClasses
+  staticClasses,
+  appDetailsClasses,
+  createReactTreePatcher,
+  findInReactTree,
+  afterPatch,
 } from "@decky/ui";
 import {
-  addEventListener,
-  removeEventListener,
-  callable,
   definePlugin,
-  toaster,
-  // routerHook
+  routerHook,
 } from "@decky/api"
-import { useState } from "react";
 import { FaShip } from "react-icons/fa";
 
-// import logo from "../assets/logo.png";
-
-// This function calls the python function "add", which takes in two numbers and returns their sum (as a number)
-// Note the type annotations:
-//  the first one: [first: number, second: number] is for the arguments
-//  the second one: number is for the return value
-const add = callable<[first: number, second: number], number>("add");
-
-// This function calls the python function "start_timer", which takes in no arguments and returns nothing.
-// It starts a (python) timer which eventually emits the event 'timer_event'
-const startTimer = callable<[], void>("start_timer");
-
-function Content() {
-  const [result, setResult] = useState<number | undefined>();
-
-  const onClick = async () => {
-    const result = await add(Math.random(), Math.random());
-    setResult(result);
-  };
-
+function SteamDBButton() {
   return (
-    <PanelSection title="Panel Section">
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={onClick}
-        >
-          {result ?? "Add two numbers via Python"}
-        </ButtonItem>
-      </PanelSectionRow>
-      <PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => startTimer()}
-        >
-          {"Start Python timer"}
-        </ButtonItem>
-      </PanelSectionRow>
-
-      {/* <PanelSectionRow>
-        <div style={{ display: "flex", justifyContent: "center" }}>
-          <img src={logo} />
-        </div>
-      </PanelSectionRow> */}
-
-      {/*<PanelSectionRow>
-        <ButtonItem
-          layout="below"
-          onClick={() => {
-            Navigation.Navigate("/decky-plugin-test");
-            Navigation.CloseSideMenus();
-          }}
-        >
-          Router
-        </ButtonItem>
-      </PanelSectionRow>*/}
-    </PanelSection>
+    <ButtonItem
+      layout="below"
+      onClick={() => console.log("SteamDB clicked")}
+    >
+      SteamDB
+    </ButtonItem>
   );
-};
+}
+
+function SettingsPanel() {
+  return (
+    <div style={{ padding: "16px" }}>
+      <ButtonItem
+        layout="below"
+        onClick={() => console.log("Clicked!")}
+      >
+        Enable Feature
+      </ButtonItem>
+    </div>
+  );
+}
 
 export default definePlugin(() => {
-  console.log("Template plugin initializing, this is called once on frontend startup")
+  const patch = routerHook.addPatch("/library/app/:appid", (tree: any) => {
+    const routeProps = findInReactTree(tree, (x: any) => x?.renderFunc);
+    if (!routeProps) return tree;
 
-  // serverApi.routerHook.addRoute("/decky-plugin-test", DeckyPluginRouterTest, {
-  //   exact: true,
-  // });
+    const patchHandler = createReactTreePatcher(
+      [
+        (innerTree: any) =>
+          findInReactTree(innerTree, (x: any) => x?.props?.children?.props?.overview)?.props?.children,
+      ],
+      (patchArgs: any[], ret?: any) => {
+        const container = findInReactTree(
+          ret,
+          (x: any) =>
+            Array.isArray(x?.props?.children) &&
+            x?.props?.className?.includes(appDetailsClasses.InnerContainer),
+        );
+        if (!container || !Array.isArray(container.props.children)) return ret;
+        const alreadyPatched = container.props.children.some(
+          (child: any) => child?.props?.["data-steamdb-button"] === "true",
+        );
+        if (!alreadyPatched) {
+          container.props.children.splice(1, 0, <SteamDBButton key="steamdb-button" />);
+        }
+        return ret;
+      },
+    );
 
-  // Add an event listener to the "timer_event" event from the backend
-  const listener = addEventListener<[
-    test1: string,
-    test2: boolean,
-    test3: number
-  ]>("timer_event", (test1, test2, test3) => {
-    console.log("Template got timer_event with:", test1, test2, test3)
-    toaster.toast({
-      title: "template got timer_event",
-      body: `${test1}, ${test2}, ${test3}`
-    });
+    afterPatch(routeProps, "renderFunc", patchHandler);
+    return tree;
   });
 
   return {
-    // The name shown in various decky menus
-    name: "Test Plugin",
-    // The element displayed at the top of your plugin's menu
-    titleView: <div className={staticClasses.Title}>Decky Example Plugin</div>,
-    // The content of your plugin's menu
-    content: <Content />,
-    // The icon displayed in the plugin list
+    name: "steamdbDeckyPlugin",
+    title: <div className={staticClasses.Title}>SteamDB Button</div>,
     icon: <FaShip />,
-    // The function triggered when your plugin unloads
+    content: <SettingsPanel />,
     onDismount() {
-      console.log("Unloading")
-      removeEventListener("timer_event", listener);
-      // serverApi.routerHook.removeRoute("/decky-plugin-test");
+      routerHook.removePatch("/library/app/:appid", patch);
     },
   };
 });
